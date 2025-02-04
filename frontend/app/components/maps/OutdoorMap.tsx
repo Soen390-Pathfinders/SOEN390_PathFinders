fae39c6b2657b0fb00ecaaca7b2d931ac37ca15a
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-02-04 04:26:58 -0500

    Cleaned up and added some comments

--- frontend/app/components/maps/OutdoorMap.tsx
+++ frontend/app/components/maps/OutdoorMap.tsx
@@ -1,27 +1,25 @@
-import MapView,{ Marker, Polygon} from 'react-native-maps';
-import { PROVIDER_GOOGLE } from 'react-native-maps';
-import {StyleSheet, Text, View, TextInput } from "react-native";
+import MapView, { Marker, Polygon } from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
+export default function OutdoorMap() {
+  //Declare the array of markers
+  const concordiaBuildings = [
+    {
+      id: 1,
+      latitude: 45.521805,
+      longitude: -73.555084,
+      title: "Hall Building",
+      description: "H Building Concordia University",
+    },
+    // Add more markers here
+  ];
 
-export default function OutdoorMap(){
-
-    //Declare the array of markers
-    const concordiaBuildings = [
-        {
-          id: 1,
-          latitude: 45.521805,
-          longitude: -73.555084,
-          title: 'Hall Building',
-          description: 'H Building Concordia University',
-        },
-        // Add more markers here
-      ];
-
-  return(
-      <View style={{flex:1}}>
+  return (
+    <View style={{ flex: 1 }}>
       <MapView
         provider={PROVIDER_GOOGLE}
-        apiKey= ' API KEY HERE'
+        /* apiKey=" API KEY HERE"*/
         style={styles.map}
         showsUserLocation={true}
         initialRegion={{
@@ -32,17 +30,19 @@ export default function OutdoorMap(){
         }}
       >
         {/*Add polygon to highlight buildings */}
-      {/*   <Polygon coordinates={
-                    [{latitude:45.521805,longitude:-73.555084},
-                     {latitude:45.531805,longitude:-73.575084},
-                     {latitude:45.551805,longitude:-73.585084},
+        <Polygon
+          coordinates={[
+            { latitude: 45.521805, longitude: -73.555084 },
+            { latitude: 45.531805, longitude: -73.575084 },
+            { latitude: 45.551805, longitude: -73.585084 },
+          ]}
+          fillColor="#27f"
+          strokeColor="#27f"
+          strokeWidth={2}
+          tappable={true}
+          onPress={() => console.log("Pressed")}
+        />
 
-                    ]}
-                    fillColor="#27f"
-                    strokeColor="#27f"
-                    strokeWidth={2}
-                    tappable={true}
-                    onPress={()=> console.log("Pressed")} />*/}
         {/* Render markers */}
         {concordiaBuildings.map((marker) => (
           <Marker
@@ -56,15 +56,15 @@ export default function OutdoorMap(){
           />
         ))}
       </MapView>
-</View>
-);
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-      map: {
-       position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0
-      },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
 });
