6250c0f069402ad25d13037925633087025e93df
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-02-06 02:13:23 -0500

    install sonarQube dependency

--- frontend/_tests_/HomeScreen.test.tsx
+++ frontend/_tests_/HomeScreen.test.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import HomeScreen from '../app/screens/HomeScreen';
-import outdoorMap from '../app/components/maps/outdoorMap.tsx'
+import React from "react";
+import renderer from "react-test-renderer";
+import outdoorMap from "../app/components/maps/outdoorMap.tsx";
+import OutdoorNav from "@/app/OutdoorNav/OutdoorNavigation";
 
-
-test('renders correctly', () => {
-  const tree = renderer.create(<HomeScreen />).toJSON();
+test("renders correctly", () => {
+  const tree = renderer.create(<OutdoorNav />).toJSON();
   expect(tree).toMatchSnapshot();
 });
