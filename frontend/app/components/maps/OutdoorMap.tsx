4dce605052f117c7f2daceacf8faf78e15a87e78
Hazem-Horus <hazem.horus.7@gmail.com>
2025-02-08 23:12:43 -0500

    addition of markers without CSS mod

--- frontend/app/components/maps/OutdoorMap.tsx
+++ frontend/app/components/maps/OutdoorMap.tsx
@@ -1,32 +1,19 @@
-//Renders the google map through react-native-maps api
+// OutdoorMap.js
 import MapView, { Marker, Polygon } from "react-native-maps";
 import { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import MapViewDirections from 'react-native-maps-directions';
-import { GOOGLE_MAPS_APIKEY } from "../../constants"
+import { GOOGLE_MAPS_APIKEY } from "../../constants";
+import { concordiaBuildings } from "./concordiaBuildings"; // Import the building data
 
 export default function OutdoorMap() {
-
-  let origin = {latitude: 45.521805, longitude: -73.555084};
-  let destination = {latitude: 45.528805, longitude: -73.555084};
-
-  //Declare the array of markers
-  const concordiaBuildings = [
-    {
-      id: 1,
-      latitude: 45.521805,
-      longitude: -73.555084,
-      title: "Hall Building",
-      description: "H Building Concordia University",
-    },
-    // Add more markers here
-  ];
+  let origin = { latitude: 45.521805, longitude: -73.555084 };
+  let destination = { latitude: 45.528805, longitude: -73.555084 };
 
   return (
     <View style={{ flex: 1 }}>
       <MapView
         provider={PROVIDER_GOOGLE}
-        /* apiKey=" API KEY HERE"*/ //GOOGLE API KEY GOES HERE
         style={styles.map}
         showsUserLocation={true}
         initialRegion={{
@@ -36,21 +23,6 @@ export default function OutdoorMap() {
           longitudeDelta: 0.0421,
         }}
       >
-        {/*Add polygon to highlight buildings //TODO: fetch the coordinate from backend*/}
-
-        <Polygon
-          coordinates={[
-            { latitude: 45.521805, longitude: -73.555084 },
-            { latitude: 45.531805, longitude: -73.575084 },
-            { latitude: 45.551805, longitude: -73.585084 },
-          ]}
-          fillColor="#27f"
-          strokeColor="#27f"
-          strokeWidth={2}
-          tappable={true}
-          onPress={() => console.log("Pressed")}
-        />
-
         {/* Render markers */}
         {concordiaBuildings.map((marker) => (
           <Marker
@@ -63,7 +35,7 @@ export default function OutdoorMap() {
             description={marker.description}
           />
         ))}
-      
+
         {/* Render MapViewDirections only if both origin and destination are set */}
         {origin && destination && (
           <MapViewDirections
@@ -79,6 +51,7 @@ export default function OutdoorMap() {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   map: {
     position: "absolute",
