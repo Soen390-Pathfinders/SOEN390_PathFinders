b45dc831ee7f2915b8e55890e17d5c29b3a9078f
aman <amansingh.sonu28@gmail.com>
2025-02-12 20:52:41 -0500

    modifed test for outdoor

--- frontend/_tests_/OutdoorDirections.test.tsx
+++ frontend/_tests_/OutdoorDirections.test.tsx
@@ -1,24 +1,27 @@
+import React from "react";
 import renderer from "react-test-renderer";
 import OutdoorDirections from "@/app/screens/OutdoorDirections";
 import { ThemeProvider } from "@/app/components/context/ThemeContext";
 import { LocationProvider } from "@/app/components/context/userLocationContext";
 
-jest.mock("@react-native-async-storage/async-storage", () => ({
+// Mock GooglePlacesAutocomplete
+jest.mock('react-native-google-places-autocomplete', () => ({
+  GooglePlacesAutocomplete: () => null
+}));
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () => ({
   setItem: jest.fn(),
-  getItem: jest.fn(() => Promise.resolve("light")),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
+  getItem: jest.fn(() => Promise.resolve('light')),
 }));
 
 test("Outdoor Directions renders correctly", () => {
-  const tree = renderer
-    .create(
-      <LocationProvider>
-        <ThemeProvider>
-          <OutdoorDirections />
-        </ThemeProvider>
-      </LocationProvider>
-    )
-    .toJSON();
+  const tree = renderer.create(
+    <LocationProvider>
+      <ThemeProvider>
+        <OutdoorDirections />
+      </ThemeProvider>
+    </LocationProvider>
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
