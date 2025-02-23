61f2e7ea47ea4a7e2cc84323e24087796b364922
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-02-23 18:33:10 -0500

    Added the snapshot for ConcordiaShuttleBus for future Integraiton testing

--- frontend/_tests_/ConcordiaShuttleTimes.test.tsx
+++ frontend/_tests_/ConcordiaShuttleTimes.test.tsx
@@ -101,3 +101,20 @@ describe("ConcordiaShuttleTimes rendering", () => {
     expect(screen.getByText("SGW CAMPUS")).toBeTruthy();
   });
 });
+
+//Add snapshot for Integration testing - making sure that nothing changes the rendering of the component in the future
+describe("ConcordiaShuttleTimes", () => {
+  test("renders the shuttle departure times screen correctly", () => {
+    const { getByText } = render(<ConcordiaShuttleTimes />);
+
+    // Check if some text or element is rendered to ensure the component renders
+    expect(getByText("Bus Shuttle Departure Times")).toBeTruthy();
+  });
+
+  test("matches snapshot", () => {
+    const { toJSON } = render(<ConcordiaShuttleTimes />);
+
+    // Create and compare snapshot
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
