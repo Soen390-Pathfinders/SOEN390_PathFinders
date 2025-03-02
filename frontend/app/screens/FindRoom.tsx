4a020dfda49f5508c108d12f14c2693d5c6bf536
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-03-02 14:39:11 -0500

    added placeholder screens for IdoorDirection, FindYourWay NavigateYourSpace and added the IdoorSirection Drawer item

---
+++ frontend/app/screens/FindRoom.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function FindRoom() {
+  return (
+    <View>
+      <Text>Find Room</Text>
+    </View>
+  );
+}
---
+++ frontend/app/screens/IndoorDirections.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function IndoorDirections() {
+  return (
+    <View>
+      <Text>Find your way </Text>
+    </View>
+  );
+}
