24de04e622fad443dba711bbc1e84c5116070e1b
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-03-08 21:06:31 -0500

    Added the circle or color for origin and destination

--- frontend/app/components/ui/pathTrace.tsx
+++ frontend/app/components/ui/pathTrace.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { useEffect, useState } from "react";
 import pathNodes from "../../data/pathNodes.json";
 import LineFactory from "@/app/hooks/lineFactory";
+import { Circle } from "react-native-svg";
 
 export default function PathTrace() {
   //Get the information from the json file
-  const myData = pathNodes; // No need for fetch
-  console.log(myData);
+  const pathInfo = pathNodes;
 
   //Get the data from the pathode Json file
   const [nodePath, setNodePath] = useState<pathNodes[]>([]);
@@ -21,16 +21,51 @@ export default function PathTrace() {
     y_coor: number;
   };
 
+  //The path will be updated every time the json file is modified
   useEffect(() => {
     setNodePath(pathNodes); // Load JSON on component mount
-  }, []);
-
-  // **Use a for loop to generate components and store them in an array**
-  const mypath = LineFactory({ nodePath });
+  }, [pathNodes]);
 
   return (
     <View>
-      {mypath} {/* Render the array of lines */}{" "}
+      {nodePath.map((node, index) => {
+        //For every node in the path, create a line to the next node using lineFactory
+        //make sure the index is not the last node
+        console.log("node", index);
+        console.log("node", index);
+        if (index === nodePath.length - 1) {
+          return (
+            <View key={node.id}>
+              {/* Create a circle for the last node */}
+              <Circle
+                cx={node.x_coor}
+                cy={node.y_coor}
+                r="0.5"
+                fill="red" // red color for the destination
+                stroke="none"
+              />
+            </View>
+          );
+        }
+        return (
+          <View>
+            {/*Create a circle for each node */}
+            <Circle
+              cx={node.x_coor}
+              cy={node.y_coor}
+              r="0.5"
+              fill={index === 0 ? "blue" : "green"}
+              stroke="none"
+            />
+            {/*LineFactory returns every line segments based on the nodes */}
+            <LineFactory
+              key={`line-${index}`}
+              node1={node}
+              node2={nodePath[index + 1]}
+            />
+          </View>
+        );
+      })}
     </View>
   );
 }
--- frontend/app/hooks/lineFactory.tsx
+++ frontend/app/hooks/lineFactory.tsx
@@ -1,32 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Line } from "react-native-svg";
+import { Line, Circle } from "react-native-svg";
 
-export default function LineFactory({ nodePath }) {
-  // Define the type of segmentsArray to hold React Elements (SVG <Line> elements)
-  const segmentsArray: JSX.Element[] = [];
-
-  for (let i = 0; i < nodePath.length - 1; i++) {
-    let x1 = nodePath[i].x_coor;
-    let y1 = nodePath[i].y_coor;
-    let x2 = nodePath[i + 1].x_coor;
-    let y2 = nodePath[i + 1].y_coor;
-    let key = "line" + i;
-    let line = (
-      <Line
-        key={key}
-        x1={x1}
-        y1={y1}
-        x2={x2}
-        y2={y2}
-        stroke="green"
-        strokeWidth="2"
-      />
-    );
-    segmentsArray.push(line);
-  }
+export default function LineFactory({ node1, node2 }) {
   return (
-    // Return the array of lines
-    segmentsArray
+    // Trace and retrun the line item for each set of 2 nodes
+    <View>
+      <Line
+        x1={node1.x_coor}
+        y1={node1.y_coor}
+        x2={node2.x_coor}
+        y2={node2.y_coor}
+        stroke="green"
+        strokeLinejoin="miter"
+        strokeWidth="0.5"
+      />
+    </View>
   );
 }
