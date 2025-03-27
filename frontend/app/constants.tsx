120f32ae15aab6f2cd5b1f660575aaac9a35f46b
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-03-27 05:21:02 -0400

    pushed my key by mistake

--- frontend/app/constants.tsx
+++ frontend/app/constants.tsx
@@ -1,2 +1,2 @@
 // constants.tsx
-export const GOOGLE_MAPS_APIKEY = "AIzaSyDC2t2ffE2zlb6v3VytPaNx2Of0R8PlVMY"; //YOUR_GOOGLE_API_KEY_HERE
+export const GOOGLE_MAPS_APIKEY = ""; //YOUR_GOOGLE_API_KEY_HERE
