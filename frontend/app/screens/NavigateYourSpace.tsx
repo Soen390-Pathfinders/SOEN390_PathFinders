7bc4f6bb6163fd28815c1b0988f05a84a0495707
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-03-28 22:10:58 -0400

    removed a console log..

--- frontend/app/screens/NavigateYourSpace.tsx
+++ frontend/app/screens/NavigateYourSpace.tsx
@@ -28,7 +28,6 @@ export default function NavigateYourSpace() {
         nodeInfo: null,
         roomOrPath: "path",
       });
-      console.log("Sending from NavigateYourSpace");
     });
   };
 
